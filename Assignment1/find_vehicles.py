343b3a8ad6eadb8d46ceed0ac589068055466d75
jess-mkg <78837964+jess-mkg@users.noreply.github.com>
2021-04-14 18:44:32 +1000

    :)

--- Assignment1/find_vehicles.py
+++ Assignment1/find_vehicles.py
@@ -1,4 +1,17 @@
 
+def boundaries(self, board, yrows, xcols, direction):
+    v_id = board[yrows,xcols]
+    v = Vehicle()
+    v.handle = v_id
+    
+    is_car = Cars().find_cars(board, yrows, xcols, direction, v, handle)
+    if is_car:
+        return is_car
+
+    is_truck = Trucks().find_trucks(board, yrows, xcols, direction, v, handle)
+    if is_truck:
+        return is_truck
+    
 
 def borders(n):
     return (n < 6 and n >= 0)
@@ -21,10 +34,12 @@ class Cars():
             next_v = 1
             vehicle.direction = 'v'
         if borders(yrows+next_h) and borders(xcols+next_v):
-            
-
-
-
+            if board[yrows+next_v,xcols+next_h] == board[yrows,xcols] == handle:
+                position = [{'x':xcols, 'y':yrows},{'x':xcols+next_h, 'y':yrows+next_v}]
+                vehicle.location = position
+                vehicle.size = 2
+                return vehicle
+        return None
 class Trucks():
     def find_trucks(self, board, yrows, xcols, direction, vehicle, handle):
         if direction == 'h':
@@ -35,25 +50,8 @@ class Trucks():
             next_h = 0
             next_v = 2
             vehicle.direction = 'v'
-
-
-
-def boundaries(self, board, yrows, xcols, direction):
-    v_id = board[yrows,xcols]
-    v = Vehicle()
-    v.handle = v_id
-    
-    is_car = Cars().find_cars(board, yrows, xcols, direction, v, handle)
-    if is_car:
-        return is_car
-
-    is_truck = Trucks().find_trucks(board, yrows, xcols, direction, v, handle)
-    if is_truck:
-        return is_truck
-    
-
-
-
+        if borders(yrows+next_v) and borders(yrows-next_v) and borders(xcols+next_h) and borders(xcols-next_h):
+            if board[yrows+next_v,xcols+next_h] == board[yrows-next_v,xcols-next_h] == handle:
 
 
 
