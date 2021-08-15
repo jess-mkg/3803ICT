1fc2e6e48c8177a2a54104e1db3a887a7d16e463
jess-mkg <78837964+jess-mkg@users.noreply.github.com>
2021-08-15 15:25:20 +1000

    Add files via upload

--- Sys.c
+++ Sys.c
@@ -1,9 +1,9 @@
+#define _GNU_SOURCE
 #include "include.h"
 #include <x86intrin.h>
-
+#include <cpuid.h>
 /* This file is called Sys.c - it contains instructions to
 retrieve OS and CPU type name and version. */
-
 void Sys()
 {
 	struct utsname uts;
@@ -17,25 +17,36 @@ void Sys()
 	for (i = 0x80000000; i <= nExIds; ++i)
 	{	
 		__cpuid(CPUInfo, i);
-	}
-	if (i == 0x80000002)
+	
+		if (i == 0x80000002)
+		{
+			memcpy(CPUBrandString,
+			CPUInfo,
+			sizeof(CPUInfo));
+		}
+		else if (i == 0x80000003)
+		{
+			memcpy(CPUBrandString + 16,
+                	CPUInfo,
+                	sizeof(CPUInfo));
+		}
+		else if (i == 0x80000004) 
+        	{
+               		memcpy(CPUBrandString + 32,
+                	CPUInfo,
+                	sizeof(CPUInfo));
+        	}
+	}*/ 
+	/*
+	FILE *cpuinfo =  fopen("/proc/cpuinfo", "rb");
+	char *arg = 0;
+	size_t size = 0;
+	while(getdelim(&arg, &size, 0, cpuinfo) != -1)
 	{
-		memcpy(CPUBrandString,
-		CPUInfo,
-		sizeof(CPUInfo));
+		puts(arg);
 	}
-	else if (i == 0x80000003)
-	{
-		memcpy(CPUBrandString + 16,
-                CPUInfo,
-                sizeof(CPUInfo));
-	}
-	else if (i == 0x80000004) 
-        {
-                memcpy(CPUBrandString + 32,
-                CPUInfo,
-                sizeof(CPUInfo));
-        }
+	free(arg);
+	fclose(cpuinfo);
 	*/
 	printf("OS name..............: %s\n", uts.sysname);
 	printf("OS version...........: %s\n", uts.version);
